67c13bf04755c8f9b9c9dc931ebfbcdd54183d6c
Josh Bialkowski <josh.bialkowski@gmail.com>
2020-02-10 07:39:15 -0800

    update to 0.6.8

--- setup.py
+++ setup.py
@@ -2,5 +2,5 @@ from setuptools import setup
 setup(
     name="cmake-format-precommit-dummy",
     version="0.0.0",
-    install_requires=["cmake-format==0.6.7"]
+    install_requires=["cmake-format==0.6.8"]
 )
